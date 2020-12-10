afd556ebb96f656981cf20098ea2d8e215b8427f
Ahmed Amr <67699360+AhmedAmr999@users.noreply.github.com>
2020-12-10 12:26:37 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,18 +1,40 @@
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<form method="POST" action="tagmy3a.php">
-		<input type="text" name="first" placeholder="FirstName" required>
-		<br>
-		<input type="text" name="last" placeholder="LastName" required>
-		<br>
-		<input type="text" name="email" placeholder="E-mail" required>
-		<br>
-		<input type="text" name="uid" placeholder="UserId" required>
-		<br>
-		<button name="submit" type="submit">SignIn</button>
-	</form>
-</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Login</title>
+  </head>
+  <body>
+
+	<form method="POST" action="tagmy3a.php" class="needs-validation" novalidate>
+		  
+		    <div class="col-md-6 mb-3">
+		      <input type="text" name="first" placeholder="ID" required>
+		      <div class="valid-tooltip">
+		        Looks good!
+		      </div>
+		    </div>
+		    <div class="col-md-6 mb-3">
+		     <input type="Password" name="pass" placeholder="Password" required>
+		      <div class="valid-tooltip">
+		        Looks good!
+		      </div>
+		    </div>
+		  	<button class="btn btn-primary" type="submit" name="submit">Submit</button>
+		  	<br>
+		  	
+    </form>
+
+   
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+  
+  </body>
 </html>
