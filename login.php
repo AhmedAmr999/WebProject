35c301dcbdcc6dd0ec6ada92ceb1a83e0e89bc7d
Ahmed Amr <67699360+AhmedAmr999@users.noreply.github.com>
2020-12-10 13:15:53 +0200

    Update login.php

--- login.php
+++ login.php
@@ -10,12 +10,12 @@
 
     <title>Login</title>
   </head>
-  <body>
-
-	<form method="POST" action="tagmy3a.php" class="needs-validation" novalidate>
-		  
+  <body class="text-center">
+<div style="margin-top: 150px;">
+	<form method="POST" action="tagmy3a.php" class="needs-validation" novalidate style="width: 100%; max-width: 330px; padding: 40px;margin: auto;">
+		  <h1 style="margin-bottom: 10px;">Please SignIn</h1>
 		    <div class="col-md-6 mb-3">
-		      <input type="text" name="first" placeholder="ID" required>
+		      <input type="text" name="id" placeholder="ID" required>
 		      <div class="valid-tooltip">
 		        Looks good!
 		      </div>
@@ -26,11 +26,11 @@
 		        Looks good!
 		      </div>
 		    </div>
-		  	<button class="btn btn-primary" type="submit" name="submit">Submit</button>
+		  	<button class="btn btn-primary" type="submit" name="signin">SignIn</button>
 		  	<br>
 		  	
     </form>
-
+</div>
    
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
